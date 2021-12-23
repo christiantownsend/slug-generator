c10c3cef83a028c51eac0865b09a75f3a84aae15
Christian Townsend <christianttownsend@gmail.com>
2021-12-22 20:30:49 -0500

    Completely revamped image export and re-styled

--- app.js
+++ app.js
@@ -51,11 +51,13 @@ Vue.component('sliders', {
 var app = new Vue({
     el: '#app',
     data: {
+        color: '#fff',
+        bgcolor: '#000',
         slug: [25, 25, 25, 25],
         club: [25, 25, 25, 25]
     },
     methods: {
-        setColors: function(dark, light, inverted) {
+        setColors: function (dark, light, inverted) {
             var r = document.querySelector(':root');
 
             r.style.setProperty('--dark-color', dark);
@@ -64,13 +66,17 @@ var app = new Vue({
             if (inverted) {
                 r.style.setProperty('--main-color', 'var(--dark-color)');
                 r.style.setProperty('--bg-color', 'var(--light-color)');
+                this.color = dark;
+                this.bgcolor = light;
             }
             else {
                 r.style.setProperty('--main-color', 'var(--light-color)');
                 r.style.setProperty('--bg-color', 'var(--dark-color)');
+                this.color = light;
+                this.bgcolor = dark;
             }
         },
-        randomizeWeights: function() {
+        randomizeWeights: function () {
 
             var randomWeight = [];
             var sum = 0;
@@ -146,14 +152,26 @@ var app = new Vue({
             const canvas = document.createElement("canvas");
             canvas.setAttribute("class", "thumbnail");
             const scale = 10;
-            Object.assign(canvas, { width: width*scale, height: height*scale });
+            Object.assign(canvas, { width: width * scale, height: height * scale });
             const ctx = canvas.getContext("2d");
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
             const thumbgrid = document.querySelector(".thumbnail-grid")
 
-            thumbgrid.appendChild( canvas );
+            thumbgrid.appendChild(canvas);
             // downloadImage(canvas.toDataURL('image/png'), 'slug-club-wordmark-variation.png')
+        },
+        genSVG2: function() {
+
+            let slug = this.slug
+            let connector = [' ']
+            let club = this.club
+
+            let weights = slug.concat(connector).concat(club).map(x => x * 14.39);
+
+            console.log(weights);
+
+            generateSVG(weights, this.color, this.bgcolor);
         }
     }
 })
@@ -202,4 +220,110 @@ function cloneWithStyles(source) {
 
     }
     return clone;
+}
+
+function generateSVG(widths, color, bgcolor) {
+    const fontUrl = "./SLUGVariableVF.ttf";
+    const slugString = "SLUG\nCLUB";
+
+    console.log(color);
+
+    fetch(fontUrl).then(res => res.arrayBuffer()).then(ab => {
+        const font = fontkit.create(new Buffer(ab));
+
+        // let fontVariation = font.getVariation({ 'wdth': 1139 });
+
+        wordmarkGlyphs = [];
+
+        for (let i = 0; i < slugString.length; i++) {
+
+            let randomWidth = Math.random() * 1139;
+
+            let fontVariation = font.getVariation({ 'wdth': widths[i] });
+
+            let run = fontVariation.layout(slugString[i]);
+
+            wordmarkGlyphs.push(run);
+        }
+
+        console.log(wordmarkGlyphs);
+
+
+        let wordmarkSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        wordmarkSVG.setAttribute('height', 300);
+        wordmarkSVG.setAttribute('width', 425);
+
+        let wordmarkGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+        wordmarkGroup.setAttribute('transform', 'scale(1 -1)');
+        wordmarkGroup.setAttribute('transform-origin', 'top left');
+
+        wordmarkSVG.appendChild(wordmarkGroup);
+
+        let x = 0;
+        // let y = 0;
+        let y = -1450;
+
+        for (let i = 0; i < wordmarkGlyphs.length; i++) {
+
+            let glyph = wordmarkGlyphs[i].glyphs[0];
+            if (glyph.id == 0) {
+                y -= 1450;
+                // y = 0;
+                x = 0;
+                continue;
+            }
+            let pos = wordmarkGlyphs[i].positions[0];
+
+            let glyphPath = document.createElementNS(
+                'http://www.w3.org/2000/svg',
+                'path'
+            );
+
+            glyphPath.setAttribute('fill', color);
+            glyphPath.setAttribute('transform', 'translate(' + x / 10 + ' ' + y / 10 + '), scale(.1) ');
+            // glyphPath.setAttribute('x', x);
+            glyphPath.setAttribute(
+                'd',
+                glyph.path.toSVG()
+            );
+
+            wordmarkGroup.appendChild(glyphPath);
+
+            console.log(glyph.path.toSVG());
+
+            x += pos.xAdvance;
+        }
+
+        // wordmarkSVGData = (new XMLSerializer()).serializeToString(wordmarkSVG);
+        // console.log(wordmarkSVG);
+        
+
+        //get svg source.
+        var serializer = new XMLSerializer();
+        var source = serializer.serializeToString(wordmarkSVG);
+
+        //add name spaces.
+        if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+            source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+        }
+        if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
+            source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+        }
+
+        //add xml declaration
+        source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+
+        //convert svg source to URI data scheme.
+        var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
+
+        const image = document.createElement('img');
+        image.setAttribute("class", "thumbnail");
+        image.setAttribute("style", "background-color: " + bgcolor + ";");
+        image.setAttribute("src", url);
+
+        const thumbgrid = document.querySelector(".thumbnail-grid")
+        thumbgrid.appendChild(image);
+
+        // return wordmarkSVG;
+    });
 }
