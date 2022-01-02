832f81f9532ad92069257f1c50c1da5327b97a09
Christian Townsend <christianttownsend@gmail.com>
2022-01-01 22:38:18 -0500

    updated styles and improved extensibility

--- app.js
+++ app.js
@@ -46,7 +46,19 @@ Vue.component('sliders', {
             this.$emit('input', this.sliders)
         }
     }
-})
+});
+
+Vue.component('color-selector', {
+    template: `
+    <button class="color-selector" :style="'--fgColor: ' + fg + '; --bgColor: ' + bg + ';'" @click="setColors(fg, bg)"></button>
+    `,
+    props: ['fg', 'bg'],
+    methods: {
+        setColors: function(fg, bg) {
+            this.$root.setColors(fg, bg);
+        }
+    }
+});
 
 var app = new Vue({
     el: '#app',
@@ -55,44 +67,38 @@ var app = new Vue({
         bgcolor: '#000',
         slug: [25, 25, 25, 25],
         club: [25, 25, 25, 25],
-        svgDataProcessed: undefined,
+        currentSVG: undefined,
     },
     computed: {
-        svgData: function() {
+        weights: function() {
             let slug = this.slug
             let connector = [' ']
             let club = this.club
 
-            let weights = slug.concat(connector).concat(club).map(x => x * 14.39);
-            
-            let data;
-
-            const url = generateSVGCode(weights, this.color);
-            url.then((response) => {
-                // console.log(response);
-                this.svgDataProcessed = response;
-            });
+            return slug.concat(connector).concat(club).map(x => x * 14.39);
         }
     },
+    watch: {
+        weights: function() {
+            // this.createSVG(this.weights, this.color).then((data) => {this.currentSVG = data;})
+            this.updateSVG();
+        },
+        color: function() {
+            this.updateSVG();
+        }
+    },
+    created: function() {
+        this.slug = [25, 25, 25, 25];
+        this.club = [25, 25, 25, 25];
+    },
     methods: {
-        setColors: function (dark, light, inverted) {
+        setColors: function (fg, bg) {
             var r = document.querySelector(':root');
 
-            r.style.setProperty('--dark-color', dark);
-            r.style.setProperty('--light-color', light);
-
-            if (inverted) {
-                r.style.setProperty('--main-color', 'var(--dark-color)');
-                r.style.setProperty('--bg-color', 'var(--light-color)');
-                this.color = dark;
-                this.bgcolor = light;
-            }
-            else {
-                r.style.setProperty('--main-color', 'var(--light-color)');
-                r.style.setProperty('--bg-color', 'var(--dark-color)');
-                this.color = light;
-                this.bgcolor = dark;
-            }
+            r.style.setProperty('--fg-color', fg);
+            r.style.setProperty('--bg-color', bg);
+            this.color = fg;
+            this.bgcolor = bg;
         },
         randomizeWeights: function () {
 
@@ -123,333 +129,112 @@ var app = new Vue({
 
             // console.log(this.slug);
         },
-        genSVG: async function () {
-            const svgNS = "http://www.w3.org/2000/svg";
-            const svg = document.createElementNS(svgNS, "svg");
-            // const reader = new FileReader()
-            const font_data = await fetchAsDataURL("./SLUGVariableVF.ttf");
-            // const font_data = "SLUGVariableVF.ttf";
-            const style = document.createElementNS(svgNS, "style");
-            style.textContent = `@font-face {
-                font-family: 'SLUG';
-                src: url(${font_data}) format('truetype'); 
-            }`;
-            svg.append(style);
+        createThumbnail: function() {
+            const image = document.createElement('img');
+            image.setAttribute("class", "thumbnail");
+            image.setAttribute("style", "background-color: " + this.bgcolor + ";");
+            image.setAttribute("src", this.currentSVG);
+            image.setAttribute("alt", "slug-club-wordmark");
 
-            const foreignObject = document.createElementNS(svgNS, "foreignObject");
-            foreignObject.setAttribute("x", 0);
-            foreignObject.setAttribute("y", 0);
+            const downloadLink = document.createElement('a');
+            // downloadLink.setAttribute('class', 'thumbnail');
+            downloadLink.setAttribute('href', this.currentSVG);
+            downloadLink.setAttribute('download', 'slug-club-wordmark.svg');
 
-            const target = document.querySelector(".target");
-            const clone = cloneWithStyles(target);
-            foreignObject.append(clone);
-
-            const {
-                width,
-                height
-            } = target.getBoundingClientRect();
-            foreignObject.setAttribute("width", width);
-            foreignObject.setAttribute("height", height);
-            svg.setAttribute("width", width);
-            svg.setAttribute("height", height);
-
-            svg.append(foreignObject);
-
-            const svg_markup = new XMLSerializer().serializeToString(svg);
-            const svg_file = new Blob([svg_markup], {
-                type: "image/svg+xml"
-            });
-
-            const img = new Image();
-
-            img.crossOrigin = 'Anonymous';
-            img.src = URL.createObjectURL(svg_file);
-            await img.decode();
-            URL.revokeObjectURL(img.src);
-
-            const canvas = document.createElement("canvas");
-            canvas.setAttribute("class", "thumbnail");
-            const scale = 10;
-            Object.assign(canvas, { width: width * scale, height: height * scale });
-            const ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            downloadLink.appendChild(image);
 
             const thumbgrid = document.querySelector(".thumbnail-grid")
-
-            thumbgrid.appendChild(canvas);
-            // downloadImage(canvas.toDataURL('image/png'), 'slug-club-wordmark-variation.png')
+            thumbgrid.appendChild(downloadLink);
         },
-        genSVG2: function() {
-
-            let slug = this.slug
-            let connector = [' ']
-            let club = this.club
-
-            let weights = slug.concat(connector).concat(club).map(x => x * 14.39);
-
-            console.log(weights);
-
-            generateSVG(weights, this.color, this.bgcolor);
-        }
+        createSVG: async function(widths, color) {
+            const fontUrl = "./SLUGVariableVF.ttf";
+            const slugString = "SLUG\nCLUB";
+        
+            return fetch(fontUrl).then(res => res.arrayBuffer()).then(ab => {
+                const font = fontkit.create(new Buffer(ab));
+        
+                wordmarkGlyphs = [];
+        
+                for (let i = 0; i < slugString.length; i++) {
+        
+                    let randomWidth = Math.random() * 1139;
+        
+                    let fontVariation = font.getVariation({ 'wdth': widths[i] });
+        
+                    let run = fontVariation.layout(slugString[i]);
+        
+                    wordmarkGlyphs.push(run);
+                }
+        
+                let wordmarkSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+                wordmarkSVG.setAttribute('height', 300);
+                wordmarkSVG.setAttribute('width', 425);
+        
+                let wordmarkGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+                wordmarkGroup.setAttribute('transform', 'scale(1 -1)');
+                wordmarkGroup.setAttribute('transform-origin', 'top left');
+        
+                wordmarkSVG.appendChild(wordmarkGroup);
+        
+                let x = 0;
+                let y = -1450;
+        
+                for (let i = 0; i < wordmarkGlyphs.length; i++) {
+        
+                    let glyph = wordmarkGlyphs[i].glyphs[0];
+                    if (glyph.id == 0) {
+                        y -= 1450;
+                        // y = 0;
+                        x = 0;
+                        continue;
+                    }
+                    let pos = wordmarkGlyphs[i].positions[0];
+        
+                    let glyphPath = document.createElementNS(
+                        'http://www.w3.org/2000/svg',
+                        'path'
+                    );
+        
+                    glyphPath.setAttribute('fill', color);
+                    glyphPath.setAttribute('transform', 'translate(' + x / 10 + ' ' + y / 10 + '), scale(.1) ');
+                    // glyphPath.setAttribute('x', x);
+                    glyphPath.setAttribute(
+                        'd',
+                        glyph.path.toSVG()
+                    );
+        
+                    wordmarkGroup.appendChild(glyphPath);
+        
+                    x += pos.xAdvance;
+                }
+                
+        
+                //get svg source.
+                var serializer = new XMLSerializer();
+                var source = serializer.serializeToString(wordmarkSVG);
+        
+                //add name spaces.
+                if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+                    source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+                }
+                if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
+                    source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+                }
+        
+                //add xml declaration
+                source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+        
+                //convert svg source to URI data scheme.
+                data = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
+        
+                return data;
+            })
+        },
+        updateSVG: function() {
+            let svg = this.createSVG(this.weights, this.color)
+            svg.then((data) => {
+                this.currentSVG = data;
+            });
+        },
     }
 })
-
-function downloadImage(data, filename = 'untitled.jpeg') {
-    var a = document.createElement('a');
-    a.href = data;
-    a.download = filename;
-    document.body.appendChild(a);
-    a.click();
-}
-
-function fetchAsDataURL(url) {
-    return fetch(url)
-        .then((resp) => resp.ok && resp.blob())
-        .then((blob) => new Promise((res) => {
-            const reader = new FileReader();
-            reader.onload = (evt) => res(reader.result);
-            reader.readAsDataURL(blob);
-        }));
-}
-
-function cloneWithStyles(source) {
-    const clone = source.cloneNode(true);
-    const source_walker = document.createTreeWalker(source, NodeFilter.SHOW_ELEMENT, null);
-    const clone_walker = document.createTreeWalker(clone, NodeFilter.SHOW_ELEMENT, null);
-    let source_element = source_walker.currentNode;
-    let clone_element = clone_walker.currentNode;
-    while (source_element) {
-
-        const source_styles = getComputedStyle(source_element);
-        const clone_styles = getComputedStyle(clone_element);
-        // not sure why but Chrome doesn't make these keys enumerable...
-        // so we have to add it ourselves
-        const style_keys = [...source_styles].concat(
-            ["font-variation-settings", "font-optical-sizing", "font-synthesis"]
-        );
-        for (let key of style_keys) {
-            if (clone_styles[key] !== source_styles[key]) {
-                clone_element.style.setProperty(key, source_styles[key]);
-            }
-        }
-
-        source_element = source_walker.nextNode()
-        clone_element = clone_walker.nextNode()
-
-    }
-    return clone;
-}
-
-function generateSVG(widths, color, bgcolor) {
-    const fontUrl = "./SLUGVariableVF.ttf";
-    const slugString = "SLUG\nCLUB";
-
-    // console.log(color);
-
-    fetch(fontUrl).then(res => res.arrayBuffer()).then(ab => {
-        const font = fontkit.create(new Buffer(ab));
-
-        // let fontVariation = font.getVariation({ 'wdth': 1139 });
-
-        wordmarkGlyphs = [];
-
-        for (let i = 0; i < slugString.length; i++) {
-
-            let randomWidth = Math.random() * 1139;
-
-            let fontVariation = font.getVariation({ 'wdth': widths[i] });
-
-            let run = fontVariation.layout(slugString[i]);
-
-            wordmarkGlyphs.push(run);
-        }
-
-        // console.log(wordmarkGlyphs);
-
-
-        let wordmarkSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        wordmarkSVG.setAttribute('height', 300);
-        wordmarkSVG.setAttribute('width', 425);
-
-        let wordmarkGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-        wordmarkGroup.setAttribute('transform', 'scale(1 -1)');
-        wordmarkGroup.setAttribute('transform-origin', 'top left');
-
-        wordmarkSVG.appendChild(wordmarkGroup);
-
-        let x = 0;
-        // let y = 0;
-        let y = -1450;
-
-        for (let i = 0; i < wordmarkGlyphs.length; i++) {
-
-            let glyph = wordmarkGlyphs[i].glyphs[0];
-            if (glyph.id == 0) {
-                y -= 1450;
-                // y = 0;
-                x = 0;
-                continue;
-            }
-            let pos = wordmarkGlyphs[i].positions[0];
-
-            let glyphPath = document.createElementNS(
-                'http://www.w3.org/2000/svg',
-                'path'
-            );
-
-            glyphPath.setAttribute('fill', color);
-            glyphPath.setAttribute('transform', 'translate(' + x / 10 + ' ' + y / 10 + '), scale(.1) ');
-            // glyphPath.setAttribute('x', x);
-            glyphPath.setAttribute(
-                'd',
-                glyph.path.toSVG()
-            );
-
-            wordmarkGroup.appendChild(glyphPath);
-
-            // console.log(glyph.path.toSVG());
-
-            x += pos.xAdvance;
-        }
-
-        // wordmarkSVGData = (new XMLSerializer()).serializeToString(wordmarkSVG);
-        // console.log(wordmarkSVG);
-        
-
-        //get svg source.
-        var serializer = new XMLSerializer();
-        var source = serializer.serializeToString(wordmarkSVG);
-
-        //add name spaces.
-        if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
-            source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
-        }
-        if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
-            source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
-        }
-
-        //add xml declaration
-        source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
-
-        //convert svg source to URI data scheme.
-        var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
-
-        const image = document.createElement('img');
-        image.setAttribute("class", "thumbnail");
-        image.setAttribute("style", "background-color: " + bgcolor + ";");
-        image.setAttribute("src", url);
-        image.setAttribute("alt", "slug-club-wordmark");
-
-        const downloadLink = document.createElement('a');
-        // downloadLink.setAttribute('class', 'thumbnail');
-        downloadLink.setAttribute('href', url);
-        downloadLink.setAttribute('download', 'slug-club-wordmark.svg');
-
-        downloadLink.appendChild(image);
-
-        const thumbgrid = document.querySelector(".thumbnail-grid")
-        thumbgrid.appendChild(downloadLink);
-
-        // return wordmarkSVG;
-    });
-}
-
-async function generateSVGCode(widths, color) {
-    const fontUrl = "./SLUGVariableVF.ttf";
-    const slugString = "SLUG\nCLUB";
-
-    // console.log(color);
-
-    return fetch(fontUrl).then(res => res.arrayBuffer()).then(ab => {
-        const font = fontkit.create(new Buffer(ab));
-
-        // let fontVariation = font.getVariation({ 'wdth': 1139 });
-
-        wordmarkGlyphs = [];
-
-        for (let i = 0; i < slugString.length; i++) {
-
-            let randomWidth = Math.random() * 1139;
-
-            let fontVariation = font.getVariation({ 'wdth': widths[i] });
-
-            let run = fontVariation.layout(slugString[i]);
-
-            wordmarkGlyphs.push(run);
-        }
-
-        // console.log(wordmarkGlyphs);
-
-
-        let wordmarkSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        wordmarkSVG.setAttribute('height', 300);
-        wordmarkSVG.setAttribute('width', 425);
-
-        let wordmarkGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-        wordmarkGroup.setAttribute('transform', 'scale(1 -1)');
-        wordmarkGroup.setAttribute('transform-origin', 'top left');
-
-        wordmarkSVG.appendChild(wordmarkGroup);
-
-        let x = 0;
-        // let y = 0;
-        let y = -1450;
-
-        for (let i = 0; i < wordmarkGlyphs.length; i++) {
-
-            let glyph = wordmarkGlyphs[i].glyphs[0];
-            if (glyph.id == 0) {
-                y -= 1450;
-                // y = 0;
-                x = 0;
-                continue;
-            }
-            let pos = wordmarkGlyphs[i].positions[0];
-
-            let glyphPath = document.createElementNS(
-                'http://www.w3.org/2000/svg',
-                'path'
-            );
-
-            glyphPath.setAttribute('fill', color);
-            glyphPath.setAttribute('transform', 'translate(' + x / 10 + ' ' + y / 10 + '), scale(.1) ');
-            // glyphPath.setAttribute('x', x);
-            glyphPath.setAttribute(
-                'd',
-                glyph.path.toSVG()
-            );
-
-            wordmarkGroup.appendChild(glyphPath);
-
-            // console.log(glyph.path.toSVG());
-
-            x += pos.xAdvance;
-        }
-
-        // wordmarkSVGData = (new XMLSerializer()).serializeToString(wordmarkSVG);
-        // console.log(wordmarkSVG);
-        
-
-        //get svg source.
-        var serializer = new XMLSerializer();
-        var source = serializer.serializeToString(wordmarkSVG);
-
-        //add name spaces.
-        if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
-            source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
-        }
-        if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
-            source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
-        }
-
-        //add xml declaration
-        source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
-
-        //convert svg source to URI data scheme.
-        data = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
-
-        // console.log(data);
-
-        return data;
-    })
-}
