52d4f3986b109fd9ed095542f6a09b2072030cae
Christian Townsend <christianttownsend@gmail.com>
2022-01-05 13:27:01 -0500

    Added app icon

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1400,
+    height: 1000,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
