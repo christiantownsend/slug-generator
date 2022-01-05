1fc767b74f584cadb90a8d572e9b665a9feedf77
Christian Townsend <christianttownsend@gmail.com>
2022-01-05 14:42:27 -0500

    Fixed dependency issue

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-if (require('electron-squirrel-startup')) return app.quit();
+if (require('electron-squirrel-startup')) return app.quit()
 
 function createWindow () {
   // Create the browser window.
